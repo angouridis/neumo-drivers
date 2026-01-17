659dd77b4ff64083d5ac57534046262db70bf02a
Deep Thought <deeptho@gmail.com>
2026-01-17 22:54:27 +0100

    Initial check in

---
+++ README.md
@@ -0,0 +1,27 @@
+TBS DRIVER
+
+Device driver for all TBS Devices DVB demodulator and modulator card/box
+
+support product
+1 pcie
+tbs6910X tbs6910 tbs6910se tbs6916 tbs6216 tbs6909x
+tbs6909SE tbs6909 tbs6908 tbs6904 tbs6904X
+tbs6904SE tbs6902 tbs6902SE tbs6590 tbs6903 tbs6903x tbs6905
+tbs6508 tbs6522 tbs6522H tbs6504 tbs7901 tbs6528 tbs6912
+tbs6504H tbs6590X 
+TBS6281 TBS6281SE TBS6205SE tbs6205 tbs6290 tbs6290se tbs7230 
+tbs6209 tbs6209SE tbs6514 tbs6814 tbs6704 
+tbs6302SE tbs6308 tbs6308X tbs6312X tbs6304 tbs6301 tbs6301se tbs6302x tbs6302T
+tbs6302RV tbs6304X tbs6304T tbs6304RV tbs6324 tbs6322 tbs690a  tbs6331
+
+tbs6900  tbs6004 tbs690b tbs6104 tbs6008 tbs6214 tbs6034 tbs6032 tbs6001
+
+2 usb
+tbs-qbox tbs-qbox2 tbs-qbox22 tbs-qboxs2 tbs-qbox2ci tbs5922se tbs5925 tbs5880
+tbs5220 tbs5230 tbs5881 tbs5520 tbs5520se tbs5530 tbs5580 tbs5927 tbs5301 tbs5930 tbs5931 tbs5590
+
+how to install the drivers?
+it just need running the script of install.sh with root permission.
+
+
+
---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root!"
+  exit
+fi
+
+echo 'Prepare system'
+
+sys=`cat /etc/os-release | grep NAME`
+ubuntu=`echo $sys | grep Ubuntu`
+centos=`echo $sys | grep CentOS`
+debian=`echo $sys | grep Debian`
+fedora=`echo $sys | grep Fedora`
+
+if [ "$centos" ] || [ "$fedora" ]; then
+        yum -y install perl-devel perl-ExtUtils-CBuilder perl-ExtUtils-MakeMaker patchutils patch
+        xx=`which gcc`
+        xx=`echo $xx | grep gcc`
+        if [ ! $xx ]; then
+                        yum install gcc
+        fi
+        xx=`which make`
+  xx=`echo $xx | grep make`
+  if [ ! $xx ]; then
+                        yum install make
+  fi
+  xx=`which unzip`
+        xx=`echo $xx | grep unzip`
+        if [ ! $xx ]; then
+                        yum install unzip
+        fi
+
+elif [ "$ubuntu" ] || [ "$debian" ]; then
+        apt-get install libproc-processtable-perl
+        apt-get install libelf-dev
+        xx=`which patch`
+  xx=`echo $xx | grep patch`
+  if [ ! $xx ]; then
+                        apt-get install patchutils patch
+  fi
+
+        xx=`which gcc`
+  xx=`echo $xx | grep gcc`
+  if [ ! $xx ]; then
+                        apt-get install gcc
+  fi
+
+  xx=`which make`
+  xx=`echo $xx | grep make`
+  if [ ! $xx ]; then
+                        apt-get install make
+  fi
+
+fi
+
+echo "V4L drivers building..."
+make -j$(nproc)
+echo "V4L drivers installing..."
+
+TARGET_MODULE="tbsecp3"
+MODULE_PATH=$(modinfo -n "$TARGET_MODULE" 2>/dev/null)
+if [ -n "$MODULE_PATH" ];  then
+	PARENT_DIR=$(dirname $(dirname $(dirname "$MODULE_PATH")))
+	rm -rf "$PARENT_DIR"
+fi
+
+sudo make install
+echo "V4L drivers installation done"
+echo "install the firmware"
+if [ -f tbs-tuner-firmwares*.tar.bz2 ]; then
+		tar jxvf tbs-tuner-firmwares*.tar.bz2 -C /lib/firmware/
+else
+		echo "firmware file not exist!!!"		
+fi
+
+echo "done. please reboot the system."
